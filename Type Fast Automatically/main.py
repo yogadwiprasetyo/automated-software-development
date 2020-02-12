b716cd1bf751debe092756589914353150058bc7
yogadwiprasetyo <yprsty01@gmail.com>
2020-02-12 22:38:06 +0700

    menambahkan file berisi automated software

---
+++ Type Fast Automatically/main.py
@@ -0,0 +1,11 @@
+# Import Package
+import TypeAuto
+
+# Insialisai Variabel
+# dengan value link
+# linkFF = 'https://10fastfingers.com/typing-test/english'
+# linkFF = 'https://10fastfingers.com/typing-test/indonesian'
+link = 'http://ngetik.maya.id/'
+
+# jalankan program
+TypeAuto.ngetik_maya(link)
