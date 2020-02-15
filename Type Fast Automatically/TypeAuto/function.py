735ee5418798705a501cb6782fc0ca8fec535955
yogadwiprasetyo <yprsty01@gmail.com>
2020-02-15 16:38:48 +0700

    merubah isi file HowToRunIt dan function

--- Type Fast Automatically/TypeAuto/function.py
+++ Type Fast Automatically/TypeAuto/function.py
@@ -74,50 +74,100 @@ def ngetik_maya(link):
 	# link yang diberikan dari argument=user
 	# memberi pesan mulai mengetik
 	browser.visit(link)
-	print('mulai mengetik')
-
+	iUser = input('Mulai mengetik ? (y/n)')
+	if iUser == 'y':
+		# Jeda
+		# Pesan mulai mengetik
 		# Looping
-	# melakukan proses waktu dan mengetik kata
-	while True:
-		# Kata
-		# jika element id cocok dengan yang dicari
-		# rubah element tersebut menjadi teks
-		# lalu masukkan value elemen ke variabel kata
-		# ketik teks dengan kata sebagai parameter
-		# lalu tekan space
-		# ulangi sampai waktu habis
-		kata = browser.find_by_id('currentword').text
-		p.typewrite(kata)
-		p.press('space')
+		# melakukan proses waktu dan mengetik kata
+		t.sleep(2)
+		print('mulai mengetik')
+		while True:
+			# Kata
+			# jika element id cocok dengan yang dicari
+			# rubah element tersebut menjadi teks
+			# lalu masukkan value elemen ke variabel kata
+			# ketik teks dengan kata sebagai parameter
+			# lalu tekan space
+			# ulangi sampai waktu habis
+			kata = browser.find_by_id('currentword').text
+			p.typewrite(kata)
+			p.press('space')
 
 
-		# Waktu 
-		# jika waktu = '0', lakukan looping
-		# looping bertujuan memproses pertanyaan yang diberikan
-		# jika pada pertanyaan tekan 'y', keluar
-		# namun jika tekan 'n', ketik kembali
-		# selain syarat diatas, beri pesan error
-		# lalu keluar browser dan program
-		waktu = browser.find_by_id('timer').text
-		if waktu == '0':
-			# while True:
-			print('selesai mengetik')
-			iUser2 = input('Keluar? (y/n): ')
-			if iUser2 is 'y':
-				print('keluar')
-				t.sleep(5)
-				browser.quit()
-				exit()
-			elif iUser2 is 'n':
-				iUser3 = input('Ketik lagi? (y): ')
-				if iUser3 is 'y':
-					browser.reload()
-					t.sleep(2)
-					# break
-				else:
-					print('ERROR: input tidak cocok')
+			# Waktu 
+			# jika waktu = '0', lakukan looping
+			# looping bertujuan memproses pertanyaan yang diberikan
+			# jika pada pertanyaan tekan 'y', keluar
+			# namun jika tekan 'n', ketik kembali
+			# selain syarat diatas, beri pesan error
+			# lalu keluar browser dan program
+			waktu = browser.find_by_id('timer').text
+			if waktu == '0':
+				# while True:
+				print('selesai mengetik')
+				iUser2 = input('Keluar? (y/n): ')
+				if iUser2 is 'y':
+					print('keluar')
+					t.sleep(5)
 					browser.quit()
 					exit()
+				elif iUser2 is 'n':
+					iUser3 = input('Ketik lagi? (y): ')
+					if iUser3 is 'y':
+						browser.reload()
+						t.sleep(2)
+					else:
+						print('ERROR: input tidak cocok')
+						browser.quit()
+						exit()
+	else:
+		print('keluar')
+		browser.quit()
+		exit()
+
+	# # Looping
+	# # melakukan proses waktu dan mengetik kata
+	# while True:
+	# 	# Kata
+	# 	# jika element id cocok dengan yang dicari
+	# 	# rubah element tersebut menjadi teks
+	# 	# lalu masukkan value elemen ke variabel kata
+	# 	# ketik teks dengan kata sebagai parameter
+	# 	# lalu tekan space
+	# 	# ulangi sampai waktu habis
+	# 	kata = browser.find_by_id('currentword').text
+	# 	p.typewrite(kata)
+	# 	p.press('space')
+
+
+	# 	# Waktu 
+	# 	# jika waktu = '0', lakukan looping
+	# 	# looping bertujuan memproses pertanyaan yang diberikan
+	# 	# jika pada pertanyaan tekan 'y', keluar
+	# 	# namun jika tekan 'n', ketik kembali
+	# 	# selain syarat diatas, beri pesan error
+	# 	# lalu keluar browser dan program
+	# 	waktu = browser.find_by_id('timer').text
+	# 	if waktu == '0':
+	# 		# while True:
+	# 		print('selesai mengetik')
+	# 		iUser2 = input('Keluar? (y/n): ')
+	# 		if iUser2 is 'y':
+	# 			print('keluar')
+	# 			t.sleep(5)
+	# 			browser.quit()
+	# 			exit()
+	# 		elif iUser2 is 'n':
+	# 			iUser3 = input('Ketik lagi? (y): ')
+	# 			if iUser3 is 'y':
+	# 				browser.reload()
+	# 				t.sleep(2)
+	# 				# break
+	# 			else:
+	# 				print('ERROR: input tidak cocok')
+	# 				browser.quit()
+	# 				exit()
 # END FUNGSI WEB NGETIK MAYA #
 
 
