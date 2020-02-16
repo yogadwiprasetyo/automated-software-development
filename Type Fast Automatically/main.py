4ba62ce97dc3a4a7f5962f7be71074a364463ed2
yprsty <yprsty01@gmail.com>
2020-02-16 11:06:55 +0700

    merubah file main

--- Type Fast Automatically/main.py
+++ Type Fast Automatically/main.py
@@ -3,8 +3,6 @@ import TypeAuto
 
 # Insialisai Variabel
 # dengan value link
-# linkFF = 'https://10fastfingers.com/typing-test/english'
-# linkFF = 'https://10fastfingers.com/typing-test/indonesian'
 link = 'http://ngetik.maya.id/'
 
 # jalankan program
